09d1a3ad4d9ef833449e53b9d699723b3e3d7e38
iankin <iankin81@gmail.com>
2012-12-05 18:33:35 +0600

    rollback

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ["DJANGO_SETTINGS_MODULE"] = "suvitatwork.settings"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suvitatwork.settings")
 
     from django.core.management import execute_from_command_line
 
